b623808619418132667ef95a7bb335879b9ba341
Bob McAleavey <bobmcaleavey@gmail.com>
2024-10-12 13:49:05 -0400

    feat: remove commented out exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,2 @@
 export { tailwindcssPaletteGenerator as default } from './tailwindcssPaletteGenerator';
-
-// export { generateColor } from './generateColor.js';
-// export { hexToHSL } from './hexToHSL.js';
-// export { hexToRGB } from './hexToRGB.js';
-// export { hslToHex } from './hslToHex.js';
 export { tailwindcssPaletteGenerator } from './tailwindcssPaletteGenerator.js';
-
-// export * from './types';
